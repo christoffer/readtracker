6578025bae4192d88ca880f4371159f7f7bb5898
Christoffer Klang <christoffer.klang@gmail.com>
2012-12-25 14:48:37 +0100

    Add stub activity for finish screen

---
+++ src/main/java/com/readtracker_beta/activities/FinishBookActivity.java
@@ -0,0 +1,11 @@
+package com.readtracker_beta.activities;
+
+import android.os.Bundle;
+import com.readtracker_beta.R;
+
+public class FinishBookActivity extends ReadTrackerActivity {
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_finish_book);
+  }
+}
