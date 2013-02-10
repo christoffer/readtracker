9860f313398f76b684f74decc505b1c1192144fc
Christoffer Klang <christoffer.klang@gmail.com>
2013-02-10 20:47:18 +0100

    Reformat LocalReading db attributes

--- src/main/java/com/readtracker/db/LocalReading.java
+++ src/main/java/com/readtracker/db/LocalReading.java
@@ -53,32 +53,66 @@ public class LocalReading implements Parcelable {
   @DatabaseField(generatedId = true)
   public int id = 0;
 
-  @DatabaseField(columnName = TITLE_FIELD_NAME) public String title;
-  @DatabaseField(columnName = AUTHOR_FIELD_NAME) public String author;
-  @DatabaseField(columnName = COVER_URL_FIELD_NAME) public String coverURL;
+  @DatabaseField(columnName = TITLE_FIELD_NAME)
+  public String title;
 
-  @DatabaseField(columnName = TOTAL_PAGES_FIELD_NAME) public long totalPages = 0;
-  @DatabaseField(columnName = CURRENT_PAGE_FIELD_NAME) public long currentPage = 0;
-  @DatabaseField(columnName = MEASURE_IN_PERCENT) public boolean measureInPercent = false;
+  @DatabaseField(columnName = AUTHOR_FIELD_NAME)
+  public String author;
 
-  @DatabaseField(columnName = PROGRESS_FIELD_NAME) public double progress = 0.0f;
-  @DatabaseField(columnName = TIME_SPENT_FIELD_NAME) public long timeSpentMillis = 0;
+  @DatabaseField(columnName = COVER_URL_FIELD_NAME)
+  public String coverURL;
 
-  @DatabaseField(columnName = LAST_READ_AT_FIELD_NAME) public long lastReadAt = 0;
-  @DatabaseField(columnName = LOCALLY_CLOSED_AT_FIELD_NAME) public long locallyClosedAt;
-  @DatabaseField(columnName = DELETED_BY_USER_FIELD_NAME) public boolean deletedByUser = false;
-  @DatabaseField(columnName = STARTED_AT_FIELD_NAME) public long startedAt = 0;
+  @DatabaseField(columnName = TOTAL_PAGES_FIELD_NAME)
+  public long totalPages = 0;
+
+  @DatabaseField(columnName = CURRENT_PAGE_FIELD_NAME)
+  public long currentPage = 0;
+
+  @DatabaseField(columnName = MEASURE_IN_PERCENT)
+  public boolean measureInPercent = false;
+
+  @DatabaseField(columnName = PROGRESS_FIELD_NAME)
+  public double progress = 0.0f;
+
+  @DatabaseField(columnName = TIME_SPENT_FIELD_NAME)
+  public long timeSpentMillis = 0;
+
+  @DatabaseField(columnName = LAST_READ_AT_FIELD_NAME)
+  public long lastReadAt = 0;
+
+  @DatabaseField(columnName = LOCALLY_CLOSED_AT_FIELD_NAME)
+  protected long locallyClosedAt;
+
+  @DatabaseField(columnName = DELETED_BY_USER_FIELD_NAME)
+  public boolean deletedByUser = false;
+
+  @DatabaseField(columnName = STARTED_AT_FIELD_NAME)
+  public long startedAt = 0;
 
   // TODO These should be in another object
-  @DatabaseField(columnName = READMILL_RECOMMENDED_FIELD_NAME) public boolean readmillRecommended = false;
-  @DatabaseField(columnName = READMILL_IS_PRIVATE_FIELD_NAME) public boolean readmillPrivate = false;
-  @DatabaseField(columnName = READMILL_READING_ID_FIELD_NAME) public long readmillReadingId = -1;
-  @DatabaseField(columnName = READMILL_BOOK_ID_FIELD_NAME) public long readmillBookId = -1;
-  @DatabaseField(columnName = READMILL_USER_ID_FIELD_NAME) public long readmillUserId = -1;
-  @DatabaseField(columnName = READMILL_TOUCHED_AT_FIELD_NAME) public long readmillTouchedAt = 0;
-  @DatabaseField(columnName = READMILL_STATE_FIELD_NAME) public int readmillState = ReadmillApiHelper.ReadingState.READING;
-  @DatabaseField(columnName = READMILL_CLOSING_REMARK) public String readmillClosingRemark = "";
+  @DatabaseField(columnName = READMILL_RECOMMENDED_FIELD_NAME)
+  public boolean readmillRecommended = false;
 
+  @DatabaseField(columnName = READMILL_IS_PRIVATE_FIELD_NAME)
+  public boolean readmillPrivate = false;
+
+  @DatabaseField(columnName = READMILL_READING_ID_FIELD_NAME)
+  public long readmillReadingId = -1;
+
+  @DatabaseField(columnName = READMILL_BOOK_ID_FIELD_NAME)
+  public long readmillBookId = -1;
+
+  @DatabaseField(columnName = READMILL_USER_ID_FIELD_NAME)
+  public long readmillUserId = -1;
+
+  @DatabaseField(columnName = READMILL_TOUCHED_AT_FIELD_NAME)
+  public long readmillTouchedAt = 0;
+
+  @DatabaseField(columnName = READMILL_STATE_FIELD_NAME)
+  public int readmillState = ReadmillApiHelper.ReadingState.READING;
+
+  @DatabaseField(columnName = READMILL_CLOSING_REMARK)
+  public String readmillClosingRemark = "";
 
   // Virtual attributes
 
