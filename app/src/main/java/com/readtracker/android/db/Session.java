39672132bb3ad724ce51824af7d0fd7af4d82242
Christoffer Klang <christoffer.klang@gmail.com>
2014-02-08 20:12:09 +0100

    Add Session model

---
+++ app/src/main/java/com/readtracker/android/db/Session.java
@@ -0,0 +1,37 @@
+package com.readtracker.android.db;
+
+import com.j256.ormlite.field.DatabaseField;
+
+/**
+ * Represents one reading session.
+ */
+public class Session {
+  @DatabaseField(columnName = "start_position") private Float mStartPosition;
+  @DatabaseField(columnName = "end_position") private Float mEndPosition;
+  @DatabaseField(columnName = "duration_seconds") private Long mDurationSeconds;
+  @DatabaseField(columnName = "started_at") private Long mStartedAt;
+  @DatabaseField(columnName = "book_id", foreign = true) private Book mBookId;
+
+  public Session() {
+  }
+
+  public Float getStartPosition() { return mStartPosition; }
+
+  public void setStartPosition(Float startPosition) { mStartPosition = startPosition; }
+
+  public Float getEndPosition() { return mEndPosition; }
+
+  public void setEndPosition(Float endPosition) { mEndPosition = endPosition; }
+
+  public Long getDurationSeconds() { return mDurationSeconds; }
+
+  public void setDurationSeconds(Long durationSeconds) { mDurationSeconds = durationSeconds; }
+
+  public Long getStartedAt() { return mStartedAt; }
+
+  public void setStartedAt(Long startedAt) { mStartedAt = startedAt; }
+
+  public Book getBookId() { return mBookId; }
+
+  public void setBookId(Book bookId) { mBookId = bookId; }
+}
