29c3d22c68af77bb0ba9dcc14a9ec1dd3f4eafd3
Christoffer Klang <christoffer.klang@gmail.com>
2014-02-15 11:01:56 +0100

    Use correct column name on Model baseclass

--- app/src/main/java/com/readtracker/android/db/Model.java
+++ app/src/main/java/com/readtracker/android/db/Model.java
@@ -6,7 +6,8 @@ import com.j256.ormlite.field.DatabaseField;
  * Base for objects that should be saved to the database.
  */
 abstract public class Model {
-  @DatabaseField(generatedId = true) private long mId;
+  @DatabaseField(generatedId = true, columnName = "id", canBeNull = false, unique = true)
+  private long mId;
 
   public long getId() {
     return mId;
