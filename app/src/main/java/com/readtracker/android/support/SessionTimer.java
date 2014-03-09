b39c1134cdce0d7e10257468b092d2a34b2dc0f4
Christoffer Klang <christoffer.klang@gmail.com>
2014-03-09 22:16:20 +0100

    Rewrite SessionTimer

--- app/src/main/java/com/readtracker/android/support/SessionTimer.java
+++ app/src/main/java/com/readtracker/android/support/SessionTimer.java
@@ -1,192 +1,115 @@
 package com.readtracker.android.support;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import com.readtracker.android.interfaces.SessionTimerEventListener;
-
-import java.util.Date;
+import android.content.SharedPreferences;
 
 /**
- * Represents a current timing session.
- * A timing session is measured by two parts:
- * - the elapsed time, in milliseconds, up until the timer was last stopped
- * - a timestamp when the timer was last started
- * <p/>
- * The timer is considered active when a timestamp since last started is present.
+ * A timer for reading sessions.
+ * Can be started and stopped and keeps track of total elapsed time.
+ * Provides convenience methods for writing or loading from a SharedPreference object.
  */
-public class SessionTimer implements Parcelable {
-  private int mLocalReadingId = -1;
-  private long mElapsedBeforeTimestamp = 0;
-  private long mActiveTimestamp = 0;
-  private SessionTimerEventListener mListener;
+public class SessionTimer {
+  private static final String PREF_TIMESTAMP = "TIMESTAMP";
+  private static final String PREF_ELAPSED = "ELAPSED";
+  private SessionTimerListener mListener;
+  private final TimeProvider mTimeProvider;
+
+  private static final long STOPPED_TIME = -1;
+
+  private long mStartTimestamp;
+  private long mElapsed;
 
   public SessionTimer() {
-    
+    this(new SystemTimeProvider());
   }
 
-  public SessionTimer(int localReadingId, long elapsedMilliseconds, long activeTimestamp) {
-    mLocalReadingId = localReadingId;
-    mElapsedBeforeTimestamp = elapsedMilliseconds;
-    mActiveTimestamp = activeTimestamp;
+  public SessionTimer(TimeProvider timeProvider) {
+    mTimeProvider = timeProvider;
   }
 
-  public SessionTimer(int localReadingId) {
-    this(localReadingId, 0, 0);
-  }
-
-  public int getLocalReadingId() {
-    return mLocalReadingId;
-  }
-
-  public long getElapsedBeforeTimestamp() {
-    return mElapsedBeforeTimestamp;
-  }
-
-  public long getActiveTimestamp() {
-    return mActiveTimestamp;
-  }
-
-  public void setEventListener(SessionTimerEventListener listener) {
+  /** Sets the listener to be notified when this timer starts or stops. */
+  public void setOnTimerListener(SessionTimerListener listener) {
     mListener = listener;
   }
 
-  /**
-   * Indicate if the timing is active or not
-   *
-   * @return true if active, false otherwise
-   */
-  public boolean isActive() {
-    return mActiveTimestamp > 0;
-  }
-
-  /**
-   * Get the total elapsed time, including any time passed since the
-   * active timestamp.
-   *
-   * @return total elapsed time in milliseconds.
-   */
-  public long getTotalElapsed() {
-    return getTotalElapsed(System.currentTimeMillis());
-  }
-
-  public long getTotalElapsed(long now) {
-    final long elapsedSinceTimestamp = isActive() ? (now - mActiveTimestamp) : 0;
-    return mElapsedBeforeTimestamp + elapsedSinceTimestamp;
-  }
-
-  public String toString() {
-    return String.format("SessionTimer for Reading#%d: Elapsed: %dms (%s)", mLocalReadingId, mElapsedBeforeTimestamp, (isActive() ? "Active, started: " + new Date(mActiveTimestamp) : "Inactive"));
-  }
-
-  // Parcelable interface
-
-  public static Parcelable.Creator<SessionTimer> CREATOR = new Parcelable.Creator<SessionTimer>() {
-    @Override
-    public SessionTimer createFromParcel(Parcel parcel) {
-      return new SessionTimer(parcel);
-    }
-
-    @Override public SessionTimer[] newArray(int size) {
-      return new SessionTimer[size];
-    }
-  };
-
-  public SessionTimer(Parcel parcel) {
-    mLocalReadingId = parcel.readInt();
-    mElapsedBeforeTimestamp = parcel.readLong();
-    mActiveTimestamp = parcel.readLong();
-  }
-
-  @Override public int describeContents() { return 0; }
-
-  @Override public void writeToParcel(Parcel parcel, int flags) {
-    parcel.writeInt(mLocalReadingId);
-    parcel.writeLong(mElapsedBeforeTimestamp);
-    parcel.writeLong(mActiveTimestamp);
-  }
-
-  /**
-   * Pauses the reading state.
-   */
-  public void stop() {
-    stop(System.currentTimeMillis());
-  }
-
-  /**
-   * Pauses the reading state.
-   */
-  public void stop(long now) {
-    stopTimer(now);
-    if(mListener != null) {
-      mListener.onStopped();
-    }
-  }
-
-  /**
-   * Start timing
-   */
+  /** Starts or resumes the timer. */
   public void start() {
-    start(System.currentTimeMillis());
-  }
-
-  public void start(long now) {
-    startTimer(now);
-    if(mListener != null) {
-      mListener.onStarted();
+    if(!isRunning()) {
+      mStartTimestamp = mTimeProvider.getTime();
     }
   }
 
-  /**
-   * Pause/resume
-   */
-  public void togglePause() {
-    togglePause(System.currentTimeMillis());
+  /** Stops the timer. */
+  public void stop() {
+    if(isRunning()) {
+      mElapsed = getElapsed();
+      mStartTimestamp = STOPPED_TIME;
+    }
   }
 
-  public void togglePause(long now) {
-    if(isActive()) {
-      stop(now);
+  /** Returns the total time elapsed. */
+  public long getElapsed() {
+    if(isRunning()) {
+      return mElapsed + (mTimeProvider.getTime() - mStartTimestamp);
     } else {
-      start(now);
+      return mElapsed;
     }
   }
 
-  /**
-   * Set the elapsed time in milliseconds.
-   *
-   * @param elapsed The elapsed time in milliseconds
-   */
-  public void setElapsedMillis(int elapsed) {
-    if(isActive()) {
-      final long now = System.currentTimeMillis();
-      stopTimer(now);
-      mElapsedBeforeTimestamp = elapsed;
-      startTimer(now);
-    } else {
-      mElapsedBeforeTimestamp = elapsed;
-    }
+  /** Returns true if the timer is currently running. */
+  public boolean isRunning() {
+    return mStartTimestamp != STOPPED_TIME;
   }
 
-  /**
-   * Stops the timer and updates the elapsed time without firing the callback.
-   *
-   * @param now Current time in epoch milliseconds
-   */
-  private void stopTimer(long now) {
-    if(isActive()) {
-      final long elapsedSinceTimeStamp = now - mActiveTimestamp;
-      mElapsedBeforeTimestamp += elapsedSinceTimeStamp;
-      mActiveTimestamp = 0;
-    }
+  /** Returns true if the timer has been started and stopped, and is currently not running. */
+  public boolean isPaused() {
+    return !isRunning() && mElapsed > 0;
   }
 
-  /**
-   * Starts the timer without firing the callback.
-   *
-   * @param now Current time in epoch milliseconds.
-   */
-  private void startTimer(long now) {
-    mActiveTimestamp = now;
+  /** Loads the timer state from preferences. Returns true if it was loaded. */
+  public boolean loadFromPreferences(SharedPreferences prefs) {
+    if(prefs.contains(PREF_ELAPSED) && prefs.contains(PREF_TIMESTAMP)) {
+      mElapsed = prefs.getLong(PREF_ELAPSED, 0);
+      mStartTimestamp = prefs.getLong(PREF_TIMESTAMP, STOPPED_TIME);
+      return true;
+    }
+    return false;
+  }
+
+
+  /** Saves the timer state to preferences. */
+  public void saveToPreferences(SharedPreferences prefs) {
+    prefs.edit()
+      .putLong(PREF_ELAPSED, mElapsed)
+      .putLong(PREF_TIMESTAMP, mStartTimestamp)
+      .commit();
+  }
+
+  /** Removes any timer state from the prefernces. */
+  public void clearFromPreferences(SharedPreferences prefs) {
+    prefs.edit()
+      .remove(PREF_ELAPSED)
+      .remove(PREF_TIMESTAMP)
+      .commit();
+  }
+
+  /** Callback interface for getting notified as the timer is started and stopped. */
+  public static interface SessionTimerListener {
+    /** Called when the timer start. */
+    public void onStarted();
+
+    /** Called when the timer stops. */
+    public void onStopped();
+  }
+
+  /** Interface for a provider of time. */
+  public static interface TimeProvider {
+    public abstract long getTime();
+  }
+
+  /** Simple TimeProvider that returns the system time. */
+  public static class SystemTimeProvider implements TimeProvider {
+    @Override public long getTime() {
+      return System.currentTimeMillis();
+    }
   }
 }
