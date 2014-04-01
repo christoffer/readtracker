73ee841fda554e8fc1ed04bf777a23e5c775764f
Christoffer Klang <christoffer.klang@gmail.com>
2014-04-01 16:10:18 +0200

    Remove dead code

--- app/src/main/java/com/readtracker/android/interfaces/ConnectedReadingListener.java
+++
@@ -1,7 +0,0 @@
-package com.readtracker.android.interfaces;
-
-import com.readtracker.android.db.LocalReading;
-
-public interface ConnectedReadingListener {
-  public void onLocalReadingConnected(LocalReading localReading);
-}
