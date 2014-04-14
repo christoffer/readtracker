ecaa52afe4659cc96b066d80fb787249ecdd1162
peter-tackage <peter.tackage@gmail.com>
2014-04-14 17:44:00 +0200

    Remove unused Intent Keys

--- app/src/main/java/com/readtracker/android/IntentKeys.java
+++ app/src/main/java/com/readtracker/android/IntentKeys.java
@@ -1,24 +1,9 @@
 package com.readtracker.android;
 
 public class IntentKeys {
-  public static final String SESSION_LENGTH_MS = "session-length";
   public static final String WEB_VIEW_URL = "webview-url";
   public static final String TITLE = "title";
   public static final String AUTHOR = "author";
   public static final String COVER_URL = "cover-url";
   public static final String PAGE_COUNT = "page-count";
-  public static final String INITIAL_FRAGMENT_PAGE = "starting-page";
-  public static final String LOCAL_READING = "local-reading";
-  public static final String READING_HIGHLIGHTS = "reading-highlights";
-  public static final String READING_SESSIONS = "reading-sessions";
-  public static final String PAGE = "page";
-  public static final String TEXT = "text";
-  public static final String LOCAL_READINGS = "local-readings";
-  public static final String RESOURCE_ID = "resource-id";
-  public static final String EDIT_MODE = "edit-mode";
-  public static final String CLOSING_REMARK = "closing-remark";
-  public static final String RECOMMENDED = "readmillRecommended";
-  public static final String FORCE_RESULT_OK = "force-result-ok";
-  public static final String SKIP_FULL_SYNC = "skip-full-sync";
-  public static final String LOCAL_HIGHLIGHT = "local-highlight";
 }
