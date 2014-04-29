d362e40d6fe2e427a75e13aef8d4b906a52dc14b
Christoffer Klang <christoffer.klang@gmail.com>
2014-04-29 08:16:42 -0700

    Tweak wait for emulator script

--- scripts/wait_for_emulator.sh
+++ scripts/wait_for_emulator.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # Lifted from: http://blog.daanraman.com/coding/automatically-detect-if-the-android-emulator-is-running/
+# and slightly modified
  
 OUT=`adb shell getprop init.svc.bootanim`
 RES="stopped"
  
 while [[ ${OUT:0:7}  != 'stopped' ]]; do
 		OUT=`adb shell getprop init.svc.bootanim`
-		echo 'Waiting for emulator to fully boot...'
-		sleep 1
+		printf "."
+		sleep 2
 done
  
 echo "Emulator booted!"
