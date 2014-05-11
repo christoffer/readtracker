b073325ec99e08d28f8ce969f550e6afe60d4a53
peter-tackage <peter.tackage@gmail.com>
2014-05-11 13:23:10 +0200

    Make BookItem members final

--- app/src/main/java/com/readtracker/android/adapters/BookItem.java
+++ app/src/main/java/com/readtracker/android/adapters/BookItem.java
@@ -4,10 +4,10 @@ package com.readtracker.android.adapters;
  * Simple display of a book with title, author and cover.
  */
 public class BookItem {
-  public String title;
-  public String author;
-  public String coverURL;
-  public long pageCount;
+  public final String title;
+  public final String author;
+  public final String coverURL;
+  public final long pageCount;
 
   public BookItem(String title, String author, String coverURL, long pageCount) {
     this.title = title;
