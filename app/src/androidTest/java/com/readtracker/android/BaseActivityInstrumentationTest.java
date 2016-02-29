357cfead008dae54d178f65ff96fa1548f49a446
Antal János Monori <anthonymonori@gmail.com>
2016-02-29 13:14:22 +0100

    renamed test class, extracted version numbers from build.gradle to rootProject build.gradle, updated gradle version to 1.5.0

--- app/src/androidTest/java/com/readtracker/android/BaseActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/readtracker/android/BaseActivityInstrumentationTest.java
@@ -7,9 +7,9 @@ import android.test.ApplicationTestCase;
  * Created by anthonymonori on 05/10/15.
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ReadTrackerAppTest extends ApplicationTestCase<Application> {
+public class BaseActivityInstrumentationTest extends ApplicationTestCase<Application> {
 
-  public ReadTrackerAppTest() {
+  public BaseActivityInstrumentationTest() {
     super(Application.class);
   }
 }
