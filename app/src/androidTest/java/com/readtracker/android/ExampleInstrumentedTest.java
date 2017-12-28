aba81708d90090d5ad87dde652009b37f06f6102
Antal János Monori <antal.janos.monori@lego.com>
2017-12-28 16:31:00 +0100

    Fixed instrumented tests: - Added the test support library and support annotations as required dependencies to run androidTest - Deleted old ReadTrackerAppTest and replaced with ExampleInstrumentedTest instead - Removed unused attributes from AndroidManifest - Removed debug suffix from debug buildType

---
+++ app/src/androidTest/java/com/readtracker/android/ExampleInstrumentedTest.java
@@ -0,0 +1,29 @@
+package com.readtracker.android;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by anthonymonori on 05/10/15.
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+  public ExampleInstrumentedTest() {}
+
+  /**
+   * Application context under test
+   */
+  @Test
+  public void usesAppContext() {
+    Context appContext = InstrumentationRegistry.getTargetContext();
+    assertEquals("com.readtracker", appContext.getPackageName());
+  }
+}
